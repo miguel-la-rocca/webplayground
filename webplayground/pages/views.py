7062674d7e0839d091d91c7d56608ca41d780c7f
Miguel La Rocca <ing.miguellarocca@gmail.com>
2022-12-20 17:32:49 -0300

    listview

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PagesListView(ListView):
+    model = Page
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+
+class PageDetailView(DetailView):
+    model = Page
