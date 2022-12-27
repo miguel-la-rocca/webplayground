e9a923a51d0dbe5ff4b93812a8383de82776e54e
Miguel La Rocca <ing.miguellarocca@gmail.com>
2022-12-27 10:29:08 -0300

    edit_ckeditor

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import PageForm
 # Create your views here.
 class PagesListView(ListView):
     model = Page
@@ -13,12 +14,12 @@ class PageDetailView(DetailView):
 
 class PageCreate(CreateView):
     model = Page
-    fields = ['title', 'content', 'order' ]
+    form_class= PageForm
     success_url= reverse_lazy('pages:pages')
 
 class PageUpdate(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order' ]
+    form_class= PageForm
     template_name_suffix = '_update_form'
     def get_success_url(self):
         return reverse_lazy ('pages:update', args=[self.object.id])+'?ok'
