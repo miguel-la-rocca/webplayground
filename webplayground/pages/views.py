1caf46f15a941ba06ddb08629710aa80cb0fa947
Miguel La Rocca <ing.miguellarocca@gmail.com>
2023-01-23 16:46:22 -0300

    StaffRequiredMixin

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -2,9 +2,18 @@ from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import PageForm
 # Create your views here.
+
+class StaffRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs) :
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
 class PagesListView(ListView):
     model = Page
 
@@ -12,12 +21,13 @@ class PagesListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class= PageForm
     success_url= reverse_lazy('pages:pages')
 
-class PageUpdate(UpdateView):
+    
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class= PageForm
     template_name_suffix = '_update_form'
@@ -25,6 +35,6 @@ class PageUpdate(UpdateView):
         return reverse_lazy ('pages:update', args=[self.object.id])+'?ok'
 
     
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
