4f835e33d6a9be15cbdd0b1a28718dee5b3a10c5
Miguel La Rocca <ing.miguellarocca@gmail.com>
2023-01-24 18:33:59 -0300

    App registration

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    #Paths de Autenticacion
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
