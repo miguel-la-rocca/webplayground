c78dc02d786da09dff14b869b88feea8f9642880
Miguel La Rocca <ing.miguellarocca@gmail.com>
2023-02-23 18:30:38 -0300

    messenger templates

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from django.http import Http404
+from .models import Thread
+#from django.shortcuts import get_object_or_404, redirect
+#from django.contrib.auth.models import User
+#from django.urls import reverse_lazy
 
 # Create your views here.
+@method_decorator(login_required, name="dispatch")
+class ThreadList(TemplateView):
+    template_name = "messenger/thread_list.html"
+
+@method_decorator(login_required, name="dispatch")
+class ThreadDetail(DetailView):
+    model = Thread
+
+    def get_object(self):
+        obj = super(ThreadDetail, self).get_object()
+        if self.request.user not in obj.users.all():
+            raise Http404()
+        return obj
