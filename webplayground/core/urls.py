edd93a999d02c3e206cadedf01472b0894712581
Miguel La Rocca <ing.miguellarocca@gmail.com>
2023-03-01 16:23:06 -0300

    reordenamiento

--- webplayground/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HomePageView, samplePageView
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('sample/', samplePageView.as_view(), name="sample"),
-]
